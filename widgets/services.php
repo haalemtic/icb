390160c53427cc5b62dbfc67cea55883eb99d0bb
haalemtic <haalemtic@gmail.com>
2022-09-23 00:42:03 +0000

    big-commit-test2

--- widgets/services.php
+++ widgets/services.php
@@ -9,7 +9,7 @@
                     <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                         <div class="service-item">
                             <div class="service-img">
-                                <img src="img/service-1.jpg" alt="Image">
+                                <img src="img/service-section-1.jpg" alt="Image">
                                 <div class="service-overlay">
                                     <p>
                                         Nous avons des ouvriers hautement qualifié en construction bâtiment pour tous vos travaux de construction ou rehabilitation de domaines.
@@ -18,14 +18,14 @@
                             </div>
                             <div class="service-text">
                                 <h3>L'Ingenierie Electrique</h3>
-                                <a class="btn" href="img/service-1.jpg" data-lightbox="service">+</a>
+                                <a class="btn" href="img/service-section-1.jpg" data-lightbox="service">+</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.2s">
                         <div class="service-item">
                             <div class="service-img">
-                                <img src="img/service-2.jpg" alt="Image">
+                                <img src="img/service-section-2.jpg" alt="Image">
                                 <div class="service-overlay">
                                     <p>
                                         Notre mission est la réalisation et / ou réhabilitation de forages d’eau, de barrages, de systèmes d’irrigation au bénéfice de l’état burkinabé et ses partenaires techniques et financiers, des collectivités territoriales et des particuliers.
@@ -34,14 +34,14 @@
                             </div>
                             <div class="service-text">
                                 <h3>L'Ingenierie Industrielle</h3>
-                                <a class="btn" href="img/service-2.jpg" data-lightbox="service">+</a>
+                                <a class="btn" href="img/service-section-2.jpg" data-lightbox="service">+</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                         <div class="service-item">
                             <div class="service-img">
-                                <img src="img/service-3.jpg" alt="Image">
+                                <img src="img/service-section-3.jpg" alt="Image">
                                 <div class="service-overlay">
                                     <p>
                                         Ceci est du faux texte à remplir Ceci est du faux texte à remplir Ceci est du
@@ -54,14 +54,14 @@
                             </div>
                             <div class="service-text">
                                 <h3>La maitrise d'oeuvre</h3>
-                                <a class="btn" href="img/service-3.jpg" data-lightbox="service">+</a>
+                                <a class="btn" href="img/service-section-3.jpg" data-lightbox="service">+</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.4s">
                         <div class="service-item">
                             <div class="service-img">
-                                <img src="img/service-4.jpg" alt="Image">
+                                <img src="img/service-section-4.jpg" alt="Image">
                                 <div class="service-overlay">
                                     <p>
                                         Ceci est du faux texte à remplir Ceci est du faux texte à remplir Ceci est du
@@ -73,14 +73,14 @@
                             </div>
                             <div class="service-text">
                                 <h3>Le contrôle technique</h3>
-                                <a class="btn" href="img/service-4.jpg" data-lightbox="service">+</a>
+                                <a class="btn" href="img/service-section-4.jpg" data-lightbox="service">+</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
                         <div class="service-item">
                             <div class="service-img">
-                                <img src="img/service-5.jpg" alt="Image">
+                                <img src="img/service-section-5.jpg" alt="Image">
                                 <div class="service-overlay">
                                     <p>
                                         Ceci est du faux texte à remplir Ceci est du faux texte à remplir Ceci est du
@@ -93,14 +93,14 @@
                             </div>
                             <div class="service-text">
                                 <h3>le conseil en maintenance et sécurité incendie</h3>
-                                <a class="btn" href="img/service-5.jpg" data-lightbox="service">+</a>
+                                <a class="btn" href="img/service-section-5.jpg" data-lightbox="service">+</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.6s">
                         <div class="service-item">
                             <div class="service-img">
-                                <img src="img/service-6.jpg" alt="Image">
+                                <img src="img/service-section-6.jpg" alt="Image">
                                 <div class="service-overlay">
                                     <p>
                                         Ceci est du faux texte à remplir Ceci est du faux texte à remplir Ceci est du
@@ -113,7 +113,7 @@
                             </div>
                             <div class="service-text">
                                 <h3>Formation</h3>
-                                <a class="btn" href="img/service-6.jpg" data-lightbox="service">+</a>
+                                <a class="btn" href="img/service-section-6.jpg" data-lightbox="service">+</a>
                             </div>
                         </div>
                     </div>
