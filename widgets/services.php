64a23ade44dafdc8ecfe95f8410783d62d2704fa
haalemtic <haalemtic@gmail.com>
2022-10-26 13:43:30 +0000

    repush

--- widgets/services.php
+++ widgets/services.php
@@ -1,123 +1,114 @@
     <!-- Service Start -->
-        <div class="service">
-            <div class="container">
-                <div class="section-header text-center">
-                    <p>Nos Services</p>
-                    <h2>Nous Fournissons ses Services</h2>
+    <div class="service">
+        <div class="container">
+            <div class="section-header text-center">
+                <p>Nos Services</p>
+                <h2>Nous Fournissons ses Services</h2>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="service-item">
+                        <div class="service-img">
+                            <img src="img/service-section-1.jpg" alt="Image">
+                            <div class="service-overlay">
+                                <p>
+                                    Nous sommes chargés de la mise en place, la maintenance et la surveillance des équipements et des installations électriques de votre entreprise.
+                                    Nous supervisons votre équipe de techniciens, nous anticipons les risques et prévoyons des solutions.Nous nous occupons également de l'organisation, de la relation client, du management, de la gestion d'un budget, des achats et des fournitures.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="service-text">
+                            <h3>L'Ingenierie Electrique</h3>
+                            <a class="btn" href="img/service-section-1.jpg" data-lightbox="service">+</a>
+                        </div>
+                    </div>
                 </div>
-                <div class="row">
-                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="service-item">
-                            <div class="service-img">
-                                <img src="img/service-section-1.jpg" alt="Image">
-                                <div class="service-overlay">
-                                    <p>
-                                        Nous avons des ouvriers hautement qualifié en construction bâtiment pour tous vos travaux de construction ou rehabilitation de domaines.
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="service-text">
-                                <h3>L'Ingenierie Electrique</h3>
-                                <a class="btn" href="img/service-section-1.jpg" data-lightbox="service">+</a>
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.2s">
+                    <div class="service-item">
+                        <div class="service-img">
+                            <img src="img/service-section-2.jpg" alt="Image">
+                            <div class="service-overlay">
+                                <p>
+                                    Nous poursuivons la réalisation de votre ouvrage destiné à la production industrielle.
+                                </p>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.2s">
-                        <div class="service-item">
-                            <div class="service-img">
-                                <img src="img/service-section-2.jpg" alt="Image">
-                                <div class="service-overlay">
-                                    <p>
-                                        Notre mission est la réalisation et / ou réhabilitation de forages d’eau, de barrages, de systèmes d’irrigation au bénéfice de l’état burkinabé et ses partenaires techniques et financiers, des collectivités territoriales et des particuliers.
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="service-text">
-                                <h3>L'Ingenierie Industrielle</h3>
-                                <a class="btn" href="img/service-section-2.jpg" data-lightbox="service">+</a>
-                            </div>
+                        <div class="service-text">
+                            <h3>L'Ingenierie Industrielle</h3>
+                            <a class="btn" href="img/service-section-2.jpg" data-lightbox="service">+</a>
                         </div>
                     </div>
-                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
-                        <div class="service-item">
-                            <div class="service-img">
-                                <img src="img/service-section-3.jpg" alt="Image">
-                                <div class="service-overlay">
-                                    <p>
-                                        Ceci est du faux texte à remplir Ceci est du faux texte à remplir Ceci est du
-                                        faux texte à remplir Ceci est du faux texte à remplir Ceci est du faux texte à
-                                        remplir Ceci est du faux texte à remplir Ceci est du faux texte à remplir Ceci
-                                        est du faux texte à remplir Ceci est du faux texte à remplir Ceci est du faux
-                                        texte à remplir.
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="service-text">
-                                <h3>La maitrise d'oeuvre</h3>
-                                <a class="btn" href="img/service-section-3.jpg" data-lightbox="service">+</a>
+                </div>
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="service-item">
+                        <div class="service-img">
+                            <img src="img/service-section-3.jpg" alt="Image">
+                            <div class="service-overlay">
+                                <p>
+                                    Nous étudions la faisabilité de votre projet ;
+                                    déterminons les spécificités du projet et adaptons le projet aux contraintes techniques et économiques ;
+                                    nous estimons le coût du projet et établissons un calendrier prévisionnel ;
+                                    nous rédigeons le dossier de consultation des entreprises ;
+                                    analysons les candidatures ;
+                                    nous effectuons un suivi régulier et rigoureux à chaque étape des travaux ;
+                                    vérifions la bonne exécution et la mise en conformité de l’ouvrage lors de la réception des travaux.
+                                </p>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.4s">
-                        <div class="service-item">
-                            <div class="service-img">
-                                <img src="img/service-section-4.jpg" alt="Image">
-                                <div class="service-overlay">
-                                    <p>
-                                        Ceci est du faux texte à remplir Ceci est du faux texte à remplir Ceci est du
-                                        faux texte à remplir Ceci est du faux texte à remplir Ceci est du faux texte à
-                                        remplir Ceci est du faux texte à remplir Ceci est du faux texte à remplir Ceci
-                                        est du faux texte à remplir Ceci est du faux texte à remplir Ceci est du faux
-                                        texte à remplir.
-                                </div>
-                            </div>
-                            <div class="service-text">
-                                <h3>Le contrôle technique</h3>
-                                <a class="btn" href="img/service-section-4.jpg" data-lightbox="service">+</a>
-                            </div>
+                        <div class="service-text">
+                            <h3>La maitrise d'oeuvre</h3>
+                            <a class="btn" href="img/service-section-3.jpg" data-lightbox="service">+</a>
                         </div>
                     </div>
-                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
-                        <div class="service-item">
-                            <div class="service-img">
-                                <img src="img/service-section-5.jpg" alt="Image">
-                                <div class="service-overlay">
-                                    <p>
-                                        Ceci est du faux texte à remplir Ceci est du faux texte à remplir Ceci est du
-                                        faux texte à remplir Ceci est du faux texte à remplir Ceci est du faux texte à
-                                        remplir Ceci est du faux texte à remplir Ceci est du faux texte à remplir Ceci
-                                        est du faux texte à remplir Ceci est du faux texte à remplir Ceci est du faux
-                                        texte à remplir.
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="service-text">
-                                <h3>le conseil en maintenance et sécurité incendie</h3>
-                                <a class="btn" href="img/service-section-5.jpg" data-lightbox="service">+</a>
+                </div>
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.4s">
+                    <div class="service-item">
+                        <div class="service-img">
+                            <img src="img/service-section-4.jpg" alt="Image">
+                            <div class="service-overlay">
+                                <p>
+                                    Nous effectuons le contrôle technique de tous vos équipements et assurons une durée d'utilisation maximale de votre équipement pour un meilleur taux de rendement.
                             </div>
                         </div>
+                        <div class="service-text">
+                            <h3>Le contrôle technique</h3>
+                            <a class="btn" href="img/service-section-4.jpg" data-lightbox="service">+</a>
+                        </div>
                     </div>
-                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.6s">
-                        <div class="service-item">
-                            <div class="service-img">
-                                <img src="img/service-section-6.jpg" alt="Image">
-                                <div class="service-overlay">
-                                    <p>
-                                        Ceci est du faux texte à remplir Ceci est du faux texte à remplir Ceci est du
-                                        faux texte à remplir Ceci est du faux texte à remplir Ceci est du faux texte à
-                                        remplir Ceci est du faux texte à remplir Ceci est du faux texte à remplir Ceci
-                                        est du faux texte à remplir Ceci est du faux texte à remplir Ceci est du faux
-                                        texte à remplir.
-                                    </p>
-                                </div>
+                </div>
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="service-item">
+                        <div class="service-img">
+                            <img src="img/service-section-5.jpg" alt="Image">
+                            <div class="service-overlay">
+                                <p>
+                                    Nos techniciens sont formés et qualifiés pour assurer la maintenance et de toutes les marques d’équipements sur vos sites et sont équipés d’outils professionnels performants pour les assister dans leur intervention.
+                                </p>
                             </div>
-                            <div class="service-text">
-                                <h3>Formation</h3>
-                                <a class="btn" href="img/service-section-6.jpg" data-lightbox="service">+</a>
+                        </div>
+                        <div class="service-text">
+                            <h3>le conseil en maintenance et sécurité incendie</h3>
+                            <a class="btn" href="img/service-section-5.jpg" data-lightbox="service">+</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.6s">
+                    <div class="service-item">
+                        <div class="service-img">
+                            <img src="img/service-section-6.jpg" alt="Image">
+                            <div class="service-overlay">
+                                <p>
+                                    Nous formons dans le domaine de l'ingenierie electrique et industrielle
+                                </p>
                             </div>
+                        </div>
+                        <div class="service-text">
+                            <h3>Formation</h3>
+                            <a class="btn" href="img/service-section-6.jpg" data-lightbox="service">+</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- Service End -->
+    </div>
+    <!-- Service End -->
